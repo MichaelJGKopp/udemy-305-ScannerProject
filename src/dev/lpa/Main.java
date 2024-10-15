bc7f575bf54c27f32464dcbb0ce53cede7b5cee4
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-15 22:58:07 +0200

    useDelimiter $ and tokens() to stream

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -10,10 +10,12 @@ public class Main {
 
     // work with different inputs in a standardized way
     try (Scanner scanner = new Scanner(new File("file.txt"))) {
-
-      while (scanner.hasNext()) {
-        System.out.println(scanner.nextLine());
-      }
+//      while (scanner.hasNext()) {
+//        System.out.println(scanner.nextLine());
+//      }
+      System.out.println(scanner.delimiter());
+      scanner.useDelimiter("$");   // there is no $ so only 1 token
+      scanner.tokens().forEach(System.out::println);
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);
     }
