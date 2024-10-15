648cf1e04f6fbd0471e2eb8deb3e5090941f476e
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-15 22:47:51 +0200

    use Scanner with File, read line-by-line

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,22 @@
 package dev.lpa;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    // work with different inputs in a standardized way
+    try (Scanner scanner = new Scanner(new File("file.txt"))) {
+
+      while (scanner.hasNext()) {
+        System.out.println(scanner.nextLine());
+      }
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
 
   }
 }
